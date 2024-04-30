b1dea289a5d3238892a2a1bdac943301bff265a2
Doug Wall <dougwall85@gmail.com>
2024-04-30 09:24:09 -0400

    Initial Commit

---
+++ Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Doug Wall on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
